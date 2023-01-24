4b016e8306642a76233f702f779015b84b42c689
Lavan Kumar <11154769+lavanganji@users.noreply.github.com>
2023-01-23 23:34:24 -0600

    sum program

---
+++ src/dataTypeExamples.java
@@ -0,0 +1,16 @@
+import javax.xml.validation.Validator;
+
+public class dataTypeExamples{
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);;
+        
+        char ch='a';
+        System.out.println(ch);
+
+        boolean boolVariable  = false;
+        System.out.println(boolVariable);
+
+        
+    }
+}
